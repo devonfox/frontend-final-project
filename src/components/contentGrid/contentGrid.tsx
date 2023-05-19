182f6e71f1b0920395622215316c1bd488927194
Ryan Filgas <rfilgas@pdx.edu>
2023-05-19 01:48:02 -0700

    remove red border

--- src/components/contentGrid/contentGrid.tsx
+++ src/components/contentGrid/contentGrid.tsx
@@ -8,9 +8,6 @@ const ContentGrid = ({myCharts}: {myCharts: chartListType}) => {
             gap={myCharts.gap}
             flexWrap={'wrap'}
             flexDirection={'row'}
-            borderStyle={'dotted'}
-            borderWidth={'2px'}
-            borderColor={'red'}
             justify={'center'}>
                   {myCharts.stockCharts.map((stockChart: chartType) => (
                         <Box w={'fit-content'} h={'fit-content'} key={stockChart.id}>
