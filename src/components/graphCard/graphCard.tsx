8d1d14e599038500b0c6417ed8f24babacc46eb7
Ryan Filgas <rfilgas@pdx.edu>
2023-05-20 13:47:31 -0700

    add scale fade to graph card

--- src/components/graphCard/graphCard.tsx
+++ src/components/graphCard/graphCard.tsx
@@ -1,44 +1,51 @@
-import React, { useState } from 'react';
-import { PieChart, ShowChart } from '@mui/icons-material';
-import { toggleCardType } from '@/types';
-import { Box } from '@chakra-ui/react'
+import React, { useState } from "react";
+import { PieChart, ShowChart } from "@mui/icons-material";
+import { toggleCardType } from "@/types";
+import { Box, ScaleFade } from "@chakra-ui/react";
 
-const GraphCard = ({myCharts}: {myCharts: toggleCardType}) => {
-    const [cardSwap, setCardSwap] = useState(false);
+const GraphCard = ({ myCharts }: { myCharts: toggleCardType }) => {
+  const [cardSwap, setCardSwap] = useState(false);
 
-    const handleClick = () => {
-        setCardSwap(!cardSwap);
-    };
+  const handleClick = () => {
+    setCardSwap(!cardSwap);
+  };
 
-    const lineChart = myCharts.lineChart;
-    const pieChart = myCharts.pieChart;
-    const lineIcon = <ShowChart onClick={handleClick}></ShowChart>
-    const pieIcon = <PieChart onClick={handleClick}></PieChart>
+  const lineChart = myCharts.lineChart;
+  const pieChart = myCharts.pieChart;
+  const lineIcon = <ShowChart onClick={handleClick}></ShowChart>;
+  const pieIcon = <PieChart onClick={handleClick}></PieChart>;
 
-    return (
+  return (
     <Box
-        color={'white'}
-        position={'relative'}
-        w={'fit-content'}
-        h={'fit-content'}>
+      color={"white"}
+      position={"relative"}
+      w={"fit-content"}
+      h={"fit-content"}
+    >
+      <ScaleFade in={cardSwap} initialScale={0.9}>
         {cardSwap && pieChart}
+      </ScaleFade>
+      <ScaleFade in={!cardSwap} initialScale={0.9}>
         {!cardSwap && lineChart}
-        <Box as='button'
-            color={'white'}
-            backgroundColor={'blue'}
-            position={'absolute'}
-            top={'10px'}
-            right={'10px'}
-            display={'flex'}
-            alignItems={'center'}
-            padding={"10px"}
-            borderRadius={'5px'}
-            border={'none'}>
-            {cardSwap && lineIcon}
-            {!cardSwap && pieIcon}
-        </Box >
+      </ScaleFade>
+      <Box
+        as="button"
+        color={"white"}
+        backgroundColor={"blue"}
+        position={"absolute"}
+        top={"10px"}
+        right={"10px"}
+        display={"flex"}
+        alignItems={"center"}
+        padding={"10px"}
+        borderRadius={"5px"}
+        border={"none"}
+      >
+        {cardSwap && lineIcon}
+        {!cardSwap && pieIcon}
+      </Box>
     </Box>
-    );
+  );
 };
 
 export default GraphCard;
