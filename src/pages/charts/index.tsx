c6128b98a68bbabf9b4e190a6702282e2b1eb36c
devonfox <devonhouston.fox@gmail.com>
2023-06-01 23:19:39 -0700

    had to fix this to test branch deploy

--- src/pages/charts/index.tsx
+++ src/pages/charts/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import ChartGrid from '@/components/chartGrid';
-import { chartListType } from '@/types';
+import React from "react";
+import { chartListType } from "@/types";
+import ContentGrid from "@/components/ContentGrid/ContentGrid";
 
 const ChartsPage = () => {
-
   const chartTestStyle: React.CSSProperties = {
-    width: '100px',
-    height: '100px',
-    backgroundColor: 'blue',
+    width: "100px",
+    height: "100px",
+    backgroundColor: "blue",
   };
 
   const exampleCharts: chartListType = {
+    gap: "1rem",
     stockCharts: [
       { id: 1, chart: <div style={chartTestStyle}></div> },
       { id: 2, chart: <div style={chartTestStyle}></div> },
@@ -30,10 +30,9 @@ const ChartsPage = () => {
   return (
     <div>
       <h1>Charts</h1>
-      <ChartGrid myCharts={exampleCharts} />
+      <ContentGrid myCharts={exampleCharts} />
     </div>
   );
 };
 
 export default ChartsPage;
-
