51baf72289493197e408d91c57dc909b18a0242e
devonfox <devonhouston.fox@gmail.com>
2023-06-03 14:54:44 -0700

    fixed merge issues + linting setup + reformatting

--- src/components/Chart/ChartPie.tsx
+++ src/components/Chart/ChartPie.tsx
@@ -1,24 +1,45 @@
 import usePieChartData from '@/hooks/usePieChartData';
 import React from 'react';
+import {
+  Cell, Legend, Pie, PieChart, ResponsiveContainer, Tooltip,
+} from 'recharts';
+import { Center, Spinner } from '@chakra-ui/react';
 
 interface PieChartProps {
   symbol: string;
+  height?: number;
 }
 
 function ChartPie(props: PieChartProps) {
-  const { symbol } = props;
+  const { symbol, height } = props;
   const { chartData, chartLoading } = usePieChartData(symbol);
 
   return !chartLoading ? (
-    <div>
-      <h1>
-        Assets: $
-        {chartData.assets.value}
-      </h1>
-    </div>
+    <ResponsiveContainer width="100%" height={height}>
+      <PieChart>
+        <Pie
+          data={chartData.pieChartData}
+          dataKey="value"
+          outerRadius={70}
+          fill="#8884d8"
+        >
+          {chartData.pieChartData.map((entry) => (
+            <Cell key={`${entry.name}`} fill={entry.color} />
+          ))}
+        </Pie>
+        <Tooltip />
+        <Legend />
+      </PieChart>
+    </ResponsiveContainer>
   ) : (
-    <div>Loading...</div>
+    <Center height={height ?? 300} width="100%">
+      <Spinner size="lg" />
+    </Center>
   );
 }
+
+ChartPie.defaultProps = {
+  height: 300,
+};
 
 export default ChartPie;
