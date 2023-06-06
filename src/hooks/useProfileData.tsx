09c65c270402031b16d9aae1eee1e94a5da356b1
devonfox <devonhouston.fox@gmail.com>
2023-06-05 21:54:04 -0700

    added profile data custom hook

---
+++ src/hooks/useProfileData.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+interface ProfileData {
+  name: string;
+  description: string;
+}
+
+const INIT_DATA: ProfileData = {
+  name: '',
+  description: '',
+};
+
+function useProfileData(symbol: string) {
+  const [profileData, setProfileData] = useState<ProfileData>(INIT_DATA);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [notFound, setNotFound] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchName = async () => {
+      const nameResponse = await fetch(`/api/polygonDetail?symbol=${symbol}`);
+      if (!nameResponse.ok) {
+        console.error(`Failed to fetch name data for ${symbol}`);
+        throw new Error('Failed to fetch name data');
+      }
+      const nameData = await nameResponse.json();
+      const { name } = nameData.results;
+      setProfileData(name);
+    };
+    fetchName().catch(() => setNotFound(true)).finally(() => { setLoading(false); });
+  }, [symbol]);
+
+
+  return { profileData, loading, notFound };
+}
+
+export default useProfileData;
